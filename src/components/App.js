595d1fec8cfe059f18aa7d497d2da14ce71a5d8f
RAJESH <108955671+rajeshmali334@users.noreply.github.com>
2022-11-23 00:06:11 +0530

    Create App.js

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component, useState} from "react";
+import '../styles/App.css';
+import Form from "./Form";
+
+const App = () => {
+  return (
+    <div id="main">
+      <Form/>
+    </div>
+  )
+}
+
+
+export default App;
